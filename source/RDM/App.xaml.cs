4518fd168fa988f0b91d6371e5677d5333a8d5a8
George Gonzalez <91168913+AvalonTeamRepo@users.noreply.github.com>
2021-10-01 20:57:38 -0700

    Initial commit

---
+++ source/RDM/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace RDM
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Save an unhandled exception.
+        /// </summary>
+        private void Application_DispatcherUnhandledException(object sender,
+            DispatcherUnhandledExceptionEventArgs e)
+        {
+            //e.Exception.Save();
+            e.Handled = true;
+        }
+        /// <summary>
+        /// Method to run when the application closes.
+        /// </summary>
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            (Process.GetCurrentProcess()).Kill();
+        }
+    }
+}
